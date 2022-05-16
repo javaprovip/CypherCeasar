e4c71502acd9bcbf785d5cdf3573a9b6821f1bb8
javaprovip <sergiokravchenko@gmail.com>
2022-05-16 10:33:13 +0300

    third

--- src/ceasar/Caesar.java
+++ src/ceasar/Caesar.java
@@ -5,6 +5,33 @@ import java.util.Scanner;
 
 public class Caesar {
 
+
+    public static void main(String[] args){
+        // Программа должна зашифровывать и расшифровывать текст, используя заданный криптографический ключ.
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter plain Text: ");
+        String plain = scan.nextLine();
+
+        System.out.print("Enter Key: ");
+        int key = scan.nextInt();
+
+        String cipherText = encoding(plain, key);
+
+        System.out.println("\nThe Cipher Text is: "+cipherText);
+
+        System.out.println("\nThe Plain Text is: "+decoding(cipherText,key));
+
+
+        //Программа должна получать путь к текстовому файлу с исходным текстом//
+        // Вопрос: как записать в файл текст и связать с файлом???
+
+        //FileWriter nFile = new FileWriter("file1.txt");
+
+        //nFile.write("Хокку \nПодобен лучу самурайский клинок \nИ тот затупился \nПроклятая килька в томате!!");
+
+        //nFile.close();//
+    }
     static String alphabet = "abcdefghijklmnopqrstuvwxyz.,””:-!?й ";
 
     public static String encoding(String pText, int key){
@@ -33,34 +60,6 @@ public class Caesar {
             pText = pText + plainChar;
         }
         return pText;
-    }
-
-
-    public static void main(String[] args){
-        // Программа должна зашифровывать и расшифровывать текст, используя заданный криптографический ключ.
-        Scanner scan = new Scanner(System.in);
-
-        System.out.print("Enter plain Text: ");
-        String plain = scan.nextLine();
-
-        System.out.print("Enter Key: ");
-        int key = scan.nextInt();
-
-        String cipherText = encoding(plain, key);
-
-        System.out.println("\nThe Cipher Text is: "+cipherText);
-
-        System.out.println("\nThe Plain Text is: "+decoding(cipherText,key));
-
-
-        //Программа должна получать путь к текстовому файлу с исходным текстом//
-        // Вопрос: как записать в файл текст и связать с файлом???
-
-        //FileWriter nFile = new FileWriter("file1.txt");
-
-        //nFile.write("Хокку \nПодобен лучу самурайский клинок \nИ тот затупился \nПроклятая килька в томате!!");
-
-        //nFile.close();//
     }
 
 }
